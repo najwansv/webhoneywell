4505a8be5bdaa86e3a9e78a29696810b53dcce90
najwansv <najwan.joan.sv@gmail.com>
2023-06-30 10:43:56 +0700

    update fetchDataRealtime

---
+++ fetchDataRealTime.php
@@ -0,0 +1,13 @@
+<?php
+    include("connectDB.php");
+    $result = mysqli_query($connectDB, "SELECT * FROM labroom ORDER BY id DESC LIMIT 1");
+    $row = mysqli_fetch_array($result);
+    $temp[] = $row["Temp"];
+    $humid[] = $row["Humid"];
+    $time[] = $row["idTime"];
+
+    // Return the data as JSON
+    $data = array("temp" => $temp, "humid" => $humid, "time" => $time);
+    echo json_encode($data);
+
+?>

2990470b5e283a4605badfe3cb3e1005cb703c7c
najwansv <najwan.joan.sv@gmail.com>
2023-06-30 10:43:03 +0700

    update SQL

--- fetchDataChart.php
+++ fetchDataChart.php
@@ -1,6 +1,6 @@
 <?php
     include("connectDB.php");
-    $result = mysqli_query($connectDB, "SELECT * FROM labroom WHERE TIME_FORMAT(idTime, '%H:%i') IN ('06:00', '09:00', '12:00', '15:00', '17:00', '19:00', '21:00', '24:00', '03:00')");
+    $result = mysqli_query($connectDB, "SELECT * FROM labroom WHERE TIME_FORMAT(idTime, '%H:%i') IN ('06:00', '09:00', '12:00', '15:00', '17:00', '19:00', '21:00', '24:00', '03:00') ORDER BY id ASC LIMIT 9");
     $row = mysqli_fetch_array($result);
 
     $temp[] = $row["Temp"];
--- index.php
+++ index.php
@@ -99,18 +99,6 @@
               </a>
             </li>
 
-            <li class="nav-item px-3 d-flex align-items-center">
-              <a href="javascript:;" class="nav-link text-body p-0">
-                <i class="fa fa-cog fixed-plugin-button-nav cursor-pointer"></i>
-              </a>
-            </li>
-
-            <li class="nav-item d-flex align-items-center">
-              <a href="pages/sign-in.html" class="nav-link text-body font-weight-bold px-0">
-                <i class="fa fa-user me-sm-1"></i>
-                <span class="d-sm-inline d-none">Sign In</span>
-              </a>
-            </li>
             
           </ul>
         </div>
@@ -122,7 +110,7 @@
     <div class="container-fluid py-4">
       <div class="row">
 
-        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+        <div class="col-xl col-sm-6 mb-xl-0 mb-4">
           <div class="card">
             <div class="card-header p-3 pt-2">
               <div class="icon icon-lg icon-shape bg-gradient-primary shadow-primary text-center border-radius-xl mt-n4 position-absolute">
@@ -140,7 +128,7 @@
           </div>
         </div>
 
-        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+        <div class="col-xl col-sm-6 mb-xl-0 mb-4">
           <div class="card">
             <div class="card-header p-3 pt-2">
               <div class="icon icon-lg icon-shape bg-gradient-success shadow-success text-center border-radius-xl mt-n4 position-absolute">
@@ -197,7 +185,7 @@
 
       <!-- charts -->
       <div class="row mt-4">
-        <div class="col-lg-4 col-md-6 mt-4 mb-4">
+        <div class="col-lg col-md-6 mt-4 mb-4">
           <div class="card z-index-2 ">
             <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
               <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
@@ -218,7 +206,7 @@
           </div>
         </div>
 
-        <div class="col-lg-4 col-md-6 mt-4 mb-4">
+        <div class="col-lg col-md-6 mt-4 mb-4">
           <div class="card z-index-2  ">
             <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
               <div class="bg-gradient-success shadow-success border-radius-lg py-3 pe-1">
@@ -239,7 +227,7 @@
           </div>
         </div>
 
-        <div class="col-lg-4 mt-4 mb-3">
+        <div class="col-lg mt-4 mb-3">
           <div class="card z-index-2 ">
             <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
               <div class="bg-gradient-dark shadow-dark border-radius-lg py-3 pe-1">
